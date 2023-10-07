e522bc3985c1cb74945b6cb3c3523365e818396b
ducloc <locdivine@gmail.com>
2023-10-07 08:17:02 +0700

    Initial commit

---
+++ app/src/main/java/com/pro/foodorder/constant/Constant.java
@@ -0,0 +1,24 @@
+package com.pro.foodorder.constant;
+
+public interface Constant {
+
+    String GENERIC_ERROR = "General error, please try again later";
+
+    String PAGE_FACEBOOK = "fb://page/lucky";
+    String LINK_FACEBOOK = "https://www.facebook.com/lucky";
+    String LINK_YOUTUBE = "https://www.youtube.com/channel/....";
+    String PHONE_NUMBER = "+84354813535";
+    String GMAIL = "lockey@gmail.com";
+    String SKYPE_ID = "lockey1807";
+    String ZALO_LINK = "https://zalo.me/0354813535";
+
+    String FIREBASE_URL = "https://food-order-a56b8-default-rtdb.firebaseio.com";
+
+    String CURRENCY = " 000 VNĐ";
+    int TYPE_PAYMENT_CASH = 1;
+    String PAYMENT_METHOD_CASH = "Tiền mặt";
+    String ADMIN_EMAIL_FORMAT = "@admin.com";
+
+    // Key Intent
+    String KEY_INTENT_FOOD_OBJECT = "food_object";
+}
